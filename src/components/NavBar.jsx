49525990cb91a0334e1f191707182c68f490e204
Ilya <honeyflym@gmail.com>
2024-08-06 21:31:24 +0300

    add file structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import { changeTabActive } from "../redux/action";
+
+const NavBar = ({ activeTab }) => {
+  const [linkNav] = useState(["home", "skills", "projects", "contacts"]);
+  const dispatch = useDispatch();
+  const changeTab = (value) => {
+    dispatch(changeTabActive(value));
+  };
+  return (
+    <header>
+      <div className="logo">
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <nav>
+        {linkNav.map((value) => (
+          <span
+            key={value}
+            className={activeTab === value ? "active" : ""}
+            onClick={() => changeTab(value)}
+          >
+            {value}
+          </span>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  activeTab: state.activeTab,
+});
+
+export default connect(mapStateToProps, { changeTabActive })(NavBar);
